b51f7b5e579f938ffcfc847d9c88c8ffa9f88878
yuanzhibang <yuanzhibang@jd.com>
2017-09-21 16:48:12 +0800

    第一次提交

---
+++ src/main/java/com/winnie/algo/App.java
@@ -0,0 +1,14 @@
+package com.winnie.algo;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+
+    }
+}
